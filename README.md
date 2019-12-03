9c3533e2a907be8994de12e1bdef6a8b3bec98af
johncondell <johncondell@gmail.com>
2019-12-03 17:15:19 +0000

    Update for blaise-dev

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Publish a files metadata onto PubSub queue when a file is uploaded to GCP bucket
+
+gcloud functions deploy helloGET \
+--source https://source.developers.google.com/projects/$PROJECT_ID/repos/hello-world/moveable-aliases/master/paths/gcf_hello_world \
+--trigger-http \
+--runtime=python37;
--- main.py
+++ main.py
@@ -2,13 +2,12 @@ def pubFileMetaData(data, context):
     import json
     from google.cloud import pubsub_v1
     project_id = "blaise-dev-258914"
-    # project_id = "blaisepoc"
-    
+
     filename = data['name']
 
     ext = data['name'].split(".")[1].lower()
     runPubSub = False
-    if (ext == "csv") :
+    if (ext == "csv"):
         runPubSub = True
         topic_name = "blaise-dev-258914-export-topic"
         sourceName = "gcp_blaise_mi"
@@ -17,7 +16,7 @@ def pubFileMetaData(data, context):
         iterationL2 = "SurveyData"
         iterationL3 = "OPN"
         iterationL4 = ""
-    elif ext == "asc" or ext == "rmk" or ext == "sps" :
+    elif ext == "asc" or ext == "rmk" or ext == "sps":
         runPubSub = True
         topic_name = "uploadedFile"
         sourceName = "gcp_blaise_dde"
@@ -30,40 +29,40 @@ def pubFileMetaData(data, context):
         runPubSub = False
         print("Error: Filetype {} not found for DDE or MI".format(ext))
 
-if (runPubSub) :
-    client = pubsub_v1.PublisherClient()
-    topic_path = client.topic_path(project_id, topic_name)
+    if (runPubSub):
+        client = pubsub_v1.PublisherClient()
+        topic_path = client.topic_path(project_id, topic_name)
 
-    version = context.event_id
-    sizeBytes = data['size']
-    md5hash = data['md5Hash']
+        version = context.event_id
+        sizeBytes = data['size']
+        md5hash = data['md5Hash']
 
-    relativePath = ".\\"
-    sensitivity = "High"
-    manifestCreated = data['timeCreated']
-    description = "GCP Blaise file trigger Manifest"
-    schemaVersion = ""
-    fullSizeMegabytes = data['size']/1000000
+        relativePath = ".\\"
+        sensitivity = "High"
+        manifestCreated = data['timeCreated']
+        description = "GCP Blaise file trigger Manifest"
+        schemaVersion = ""
+        fullSizeMegabytes = data['size']/1000000
 
-    msg = {}
-    files = {}
+        msg = {}
+        files = {}
 
-    msg["version"] = f"{version}"
-    files["sizeBytes"] = f"{sizeBytes}"
-    files["name"] = f"{filename}"
-    files["md5sum"] = f"{md5hash}"
-    files["relativePath"] = f"{relativePath}"
-    msg["files"] = f"[{files}]"
-    msg["sensitivity"] = f"{sensitivity}"
-    msg["sourceName"] = f"{sourceName}"
-    msg["manifestCreated"] = f"{manifestCreated}"
-    msg["description"] = f"{description}"
-    msg["iterationL1"] = f"{iterationL1}"
-    msg["iterationL2"] = f"{iterationL2}"
-    msg["iterationL3"] = f"{iterationL3}"
-    msg["iterationL4"] = f"{iterationL4}"
-    msg["dataset"] = f"{dataset}"
-    msg["schemaVersion"] = f"{schemaVersion}"
-    msg["fullSizeMegabytes"] = f"{fullSizeMegabytes}"
-    msgbytes = bytes(json.dumps(msg), encoding='utf-8')
-    client.publish(topic_path, data=msgbytes)
+        msg["version"] = f"{version}"
+        files["sizeBytes"] = f"{sizeBytes}"
+        files["name"] = f"{filename}"
+        files["md5sum"] = f"{md5hash}"
+        files["relativePath"] = f"{relativePath}"
+        msg["files"] = f"[{files}]"
+        msg["sensitivity"] = f"{sensitivity}"
+        msg["sourceName"] = f"{sourceName}"
+        msg["manifestCreated"] = f"{manifestCreated}"
+        msg["description"] = f"{description}"
+        msg["iterationL1"] = f"{iterationL1}"
+        msg["iterationL2"] = f"{iterationL2}"
+        msg["iterationL3"] = f"{iterationL3}"
+        msg["iterationL4"] = f"{iterationL4}"
+        msg["dataset"] = f"{dataset}"
+        msg["schemaVersion"] = f"{schemaVersion}"
+        msg["fullSizeMegabytes"] = f"{fullSizeMegabytes}"
+        msgbytes = bytes(json.dumps(msg), encoding='utf-8')
+        client.publish(topic_path, data=msgbytes)
