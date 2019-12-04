66b2759ca9b16e6cd161e62afa5c245c651e68ac
johncondell <johncondell@gmail.com>
2019-12-04 17:45:33 +0000

    Add json as templates

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Publish a files metadata onto PubSub queue when a file is uploaded to GCP bucket.
 
-gcloud functions deploy pubFileMetaData --runtime python37 --trigger-resource dde_bucket --trigger-event google.storage.object.finalize
+From GCP console, deploy function with the following
+gcloud functions deploy pubFileMetaData --runtime python37 --trigger-resource dde_bucket --trigger-event google.storage.object.finalize --set-env-vars PROJECT_ID=blaise-dev-258914
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def pubFileMetaData(data, context):
     import json
+    import os
+
     from google.cloud import pubsub_v1
-    project_id = "blaise-dev-258914"
+    project_id = os.environ['PROJECT_ID']
 
     filename = data['name']
 
@@ -10,21 +12,11 @@ def pubFileMetaData(data, context):
     if (ext == "csv"):
         runPubSub = True
         topic_name = "blaise-dev-258914-export-topic"
-        sourceName = "gcp_blaise_mi"
-        dataset = "blaise_mi"
-        iterationL1 = "ldata10"
-        iterationL2 = "SurveyData"
-        iterationL3 = "OPN"
-        iterationL4 = ""
+        metaTemplate = "mi-meta-template.json"
     elif ext == "asc" or ext == "rmk" or ext == "sps":
         runPubSub = True
         topic_name = "uploadedFile"
-        sourceName = "gcp_blaise_dde"
-        dataset = "blaise_dde"
-        iterationL1 = "ldata12"
-        iterationL2 = ""
-        iterationL3 = ""
-        iterationL4 = ""
+        metaTemplate = "dde-meta-template.json"
     else:
         runPubSub = False
         print("Error: Filetype {} not found for DDE or MI".format(ext))
@@ -33,36 +25,20 @@ def pubFileMetaData(data, context):
         client = pubsub_v1.PublisherClient()
         topic_path = client.topic_path(project_id, topic_name)
 
-        version = context.event_id
+        with open(metaTemplate) as json_file:
+            msg = json.load(json_file)
+
+        files = {}
+        filename = data['name']
         sizeBytes = data['size']
         md5hash = data['md5Hash']
-
-        relativePath = ".\\"
-        sensitivity = "High"
         manifestCreated = data['timeCreated']
-        description = "GCP Blaise file trigger Manifest"
-        schemaVersion = ""
-        fullSizeMegabytes = data['size']/1000000
+        fullSizeMegabytes = int(data['size'])/1000000
 
-        msg = {}
-        files = {}
-
-        msg["version"] = f"{version}"
         files["sizeBytes"] = f"{sizeBytes}"
-        files["name"] = f"{filename}"
+        files["name"] = f"{data['name']}"
         files["md5sum"] = f"{md5hash}"
-        files["relativePath"] = f"{relativePath}"
-        msg["files"] = f"[{files}]"
-        msg["sensitivity"] = f"{sensitivity}"
-        msg["sourceName"] = f"{sourceName}"
+        files["relativePath"] = ".\\"
+        msg['files'].append(files)
         msg["manifestCreated"] = f"{manifestCreated}"
-        msg["description"] = f"{description}"
-        msg["iterationL1"] = f"{iterationL1}"
-        msg["iterationL2"] = f"{iterationL2}"
-        msg["iterationL3"] = f"{iterationL3}"
-        msg["iterationL4"] = f"{iterationL4}"
-        msg["dataset"] = f"{dataset}"
-        msg["schemaVersion"] = f"{schemaVersion}"
         msg["fullSizeMegabytes"] = f"{fullSizeMegabytes}"
-        msgbytes = bytes(json.dumps(msg), encoding='utf-8')
-        client.publish(topic_path, data=msgbytes)
