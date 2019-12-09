8db2ba6c7c859f88ee3462c4717ecfe64461efc7
johncondell <johncondell@gmail.com>
2019-12-09 11:19:30 +0000

    LU4501 - add json templates for Mi and DDE metadata

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
+def createMsg(data, dest):
+    import json, os
+
+    with open(dest["metaTemplate"]) as json_file:
+        msg = json.load(json_file)
+
+    files = {}
+    filename = data['name'] + ":" + data['bucket']
+    sizeBytes = data['size']
+    md5hash = data['md5Hash']
+    manifestCreated = data['timeCreated']
+    fullSizeMegabytes = "{:.6f}".format(int(data['size'])/1000000)
+    files["sizeBytes"] = f"{sizeBytes}"
+    files["name"] = f"{filename}"
+    files["md5hash"] = f"{md5hash}"
+    files["relativePath"] = ".\\"
+    msg['files'] = f"{files}"
+    msg["manifestCreated"] = f"{manifestCreated}"
+    msg["fullSizeMegabytes"] = f"{fullSizeMegabytes}"
+    return msg
+
 def pubFileMetaData(data, context):
-    import json
     import os
     
     from google.cloud import pubsub_v1
     project_id = os.environ['PROJECT_ID']
     if(project_id): 
-
+        dest = {}
         topic_name = "blaise-dev-258914-export-topic"
         fileExtn = data['name'].split(".")[1].lower()
         runPubSub = False
         if (fileExtn == "csv"):
             runPubSub = True
-            metaTemplate = "mi-meta-template.json"
-            iterationL2 = ""
-            iterationL3 = ""
-            iterationL4 = ""
+            metaTemplate = os.getcwd + "\mi-meta-template.json"
+            dest["metaTemplate"] = f"{metaTemplate}"
+            dest["iterationL2"] = f"{''}"
+            dest["iterationL3"] = f"{''}"
+            dest["iterationL4"] = f"{''}"  
+
         elif fileExtn == "asc" or fileExtn == "rmk" or fileExtn == "sps":
             runPubSub = True
+            metaTemplate = os.getcwd + "\dde-meta-template.json"
             # File needs to be in the format of opn1911a.sps
-            iterationL2 = data['name'][:3]
-            iterationL3 = data['name'][3:7]
-            iterationL4 = data['name'][7:8]
-            metaTemplate = "dde-meta-template.json"
+            dest["metaTemplate"] = f"{metaTemplate}"
+            dest["iterationL2"] = f"{data['name'][:3]}"
+            dest["iterationL3"] = f"{data['name'][3:7]}"
+            dest["iterationL4"] = f"{data['name'][7:8]}"  
         else:
             runPubSub = False
             print("Error: Filetype {} not found for DDE or MI".format(fileExtn))
@@ -29,27 +52,6 @@ def pubFileMetaData(data, context):
         if (runPubSub):
             client = pubsub_v1.PublisherClient()
             topic_path = client.topic_path(project_id, topic_name)
-
-            with open(metaTemplate) as json_file:
-                msg = json.load(json_file)
-
-            files = {}
-            filename = data['name'] + ":" + data['bucket']
-            sizeBytes = data['size']
-            md5hash = data['md5Hash']
-            manifestCreated = data['timeCreated']
-            fullSizeMegabytes = "{:.6f}".format(int(data['size'])/1000000)
-            files["sizeBytes"] = f"{sizeBytes}"
-            files["name"] = f"{filename}"
-            files["md5hash"] = f"{md5hash}"
-            files["relativePath"] = ".\\"
-            msg['files'].append(files)
-            msg["iterationL2"] = f"{iterationL2}"
-            msg["iterationL3"] = f"{iterationL3}"
-            msg["iterationL4"] = f"{iterationL4}"
-            msg["manifestCreated"] = f"{manifestCreated}"
-            msg["fullSizeMegabytes"] = f"{fullSizeMegabytes}"
-
+            msg = createMsg(data, dest)
             msgbytes = bytes(json.dumps(msg), encoding='utf-8')
             client.publish(topic_path, data=msgbytes)
-            print (json.dumps(msg, sort_keys=True, indent=3))
--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-import json
-from main import *
-with open("c:\gcp\pubMeta\data-sent-by-gcp-to-storage-func.json") as json_file:
-    datatest = json.load(json_file)
+import json, os
+from main import createMsg
+with open(os.getcwd() + "\data-sent-by-gcp-to-storage-func.json") as json_file:
+    data = json.load(json_file)
 
-pubFileMetaData(datatest)          
-           
+dest = {}
+
+metaTemplate = os.getcwd() + "\mi-meta-template.json"
+dest["metaTemplate"] = f"{metaTemplate}"
+dest["iterationL2"] = f"{data['name'][:3]}"
+dest["iterationL3"] = f"{data['name'][3:7]}"
+dest["iterationL4"] = f"{data['name'][7:8]}"    
+
+x = createMsg(data,dest)       
+print(json.dumps(x, indent=3))
