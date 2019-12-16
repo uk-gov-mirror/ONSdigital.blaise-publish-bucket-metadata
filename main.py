fb14e169518842dd386ae519bb784ce60f51a107
johncondell <johncondell@gmail.com>
2019-12-16 11:29:52 +0000

    Add logging

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 def encrypt_file(filename):
     import os
     import gnupg
+    from pprint import pprint
+
     gpghome = os.path.join(os.getcwd(),'gpghome')
     os.system('rm -rf ' + gpghome)
     os.system('mkdir '+ gpghome)
     gpg = gnupg.GPG(gnupghome=gpghome)
     pubkey = open('key.gpg').read()
     import_result = gpg.import_keys(pubkey)
-    # print(import_result.results)
+    pprint(import_result.results)
+    print(import_result.results)
     uid = "blaise5-gcp-gpg-key (key to be used for blaise5 content) <nic.hayes@ons.statistics.gov.uk>"
-    open(filename, 'w').write('You need to Google Venn diagram.')
     with open(filename, 'rb') as f:
-        status = gpg.encrypt_file( f, recipients=[uid],sign=None,armor=True, output=filename,always_trust=True)
+        status = gpg.encrypt_file(f, recipients=[uid], sign=None, armor=True, output=filename, always_trust=True)
+    print ('jc ok: ', status.ok)
+    print ('jc status: ', status.status)
+    print ('jc stderr: ', status.stderr)
 
 
 def createMsg(data, dest):
