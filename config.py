698f3876e4ac7f7df5c2f2349d014c1e2dbef573
elthorne <46898565+elthorne@users.noreply.github.com>
2021-04-16 10:52:10 +0100

    Added dataclass to remove use of getenv in publishMsg()

---
+++ config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import os
+
+
+@dataclass
+class Config(object):
+    on_prem_subfolder: str
+    project_id: str
+    topic_name: str
+    env: str
+
+    @classmethod
+    def from_env(cls):
+        return cls(
+            on_prem_subfolder=os.getenv('ON-PREM-SUBFOLDER'),
+            project_id=os.getenv("PROJECT_ID"),
+            topic_name=os.getenv("TOPIC_NAME"),
+            env=os.getenv("ENV")
+        )
