0a1f7cdff0a919db2582eeb892fa34a46b6c911b
elthorne <46898565+elthorne@users.noreply.github.com>
2021-04-20 18:32:10 +0100

    TODO message to remind us to...well, DO stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -71,6 +71,7 @@ def expected_pubsub_message_opn():
 
 @pytest.fixture
 def expected_pubsub_message_lms():
+    # TODO: I think we need a different dataset for LMS stuff for NiFi to know about ndata3 (make it up)
     return {
         "version": 3,
         "schemaVersion": 1,
